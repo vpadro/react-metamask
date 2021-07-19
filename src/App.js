4b5df2a516426f6e2269bf96a308626f750e3c98
vpadro <vpadro91@gmail.com>
2021-07-19 08:17:41 +0200

    deployed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useStoreApi } from "./storeApi";
+import useWeb3 from "./useWeb3";
+
+
+import "./App.css";
+import EthLogo from "./img/Eth.png";
 
 function App() {
+  const { balance, address, message, setAddress,  } = useStoreApi();
+  const web3 = useWeb3();
+
+  // get user account on button click
+  const getUserAccount = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.enable();
+        web3.eth.getAccounts().then(accounts => {
+          setAddress(accounts[0]);
+       
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      alert("Nie wykryto rozszerzenia MetaMask!");
+    }
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={EthLogo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        
+        {address ? (
+          <>
+            <p> Adres konta {address}</p>
+            <p> Saldo: {balance} </p>
+          </>
+        ) : null}
+        <button
+          onClick={() => getUserAccount()}
+          variant="outlined"
+          color="primary"
+          class="button"
         >
-          Learn React
-        </a>
+         Połącz z MetaMask
+        </button>
+        {message ? (
+          <p>
+            <code>{message}</code>
+          </p>
+        ) : null}
       </header>
     </div>
   );
